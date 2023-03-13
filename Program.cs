d00eb60ca1c724e321fd1aefd0778ddf600f16b9
Макс <prostomax01@icloud.com>
2023-03-13 14:08:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    // Контекст определяет интерфейс, представляющий интерес для клиентов.
+    class Context
+    {
+        // Контекст хранит ссылку на один из объектов Стратегии. Контекст не
+        // знает конкретного класса стратегии. Он должен работать со всеми
+        // стратегиями через интерфейс Стратегии.
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        // Обычно Контекст принимает стратегию через конструктор, а также
+        // предоставляет сеттер для её изменения во время выполнения.
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Обычно Контекст позволяет заменить объект Стратегии во время
+        // выполнения.
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Вместо того, чтобы самостоятельно реализовывать множественные версии
+        // алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Сортировка");
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    // Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых
+    // версий некоторого алгоритма.
+    //
+    // Контекст использует этот интерфейс для вызова алгоритма, определённого
+    // Конкретными Стратегиями.
+    public interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код выбирает конкретную стратегию и передаёт её в
+            // контекст. Клиент должен знать о различиях между стратегиями,
+            // чтобы сделать правильный выбор.
+            var context = new Context();
+
+            Console.WriteLine("Client: Сортировка прошла успешна");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Обратная сортировка.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
+}
